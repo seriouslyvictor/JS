5101ab22534bbf1fc6b17c3dcddaead1946b9275
Victor Schiavo <schiavovictor@outlook.com>
2022-07-11 14:10:29 -0300

    Incluido tips2.js

--- Fundamentos 2/arrays.js
+++ Fundamentos 2/arrays.js
@@ -28,3 +28,35 @@ function calculaIdade(nascimento) {
 const idades = [calculaIdade(anos[0]), calculaIdade(anos[1]), calculaIdade(anos[2]), calculaIdade(anos[anos.length - 1])]
 console.log(idades);
 
+
+// Adicionando elementos ao Array
+amigos.push(`Tibúrcio`)
+console.log(amigos);
+// Adiciona elementos no fim do array
+
+
+amigos.unshift('Curupira')
+console.log(amigos);
+// Adiciona elementos no começo do array
+
+// Removendo elementos do Array
+amigos.pop(); //remove o último elemento.
+const popped = amigos.pop();
+console.log(popped);
+console.log(amigos);
+
+amigos.shift(); //Remove o primeiro elemento do array,
+const shifted = amigos.shift();
+console.log(shifted);
+console.log(amigos);
+
+console.log(amigos.indexOf('Edão'))
+console.log(amigos.indexOf('Curupira'))
+
+
+console.log(amigos.includes('Edão'))
+console.log(amigos.includes('Curupira'))
+
+
+
+
