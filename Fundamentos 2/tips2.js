d611cf02f674f94616ebb570ad24c9c3754f9943
Victor Schiavo <schiavovictor@outlook.com>
2022-07-11 14:21:38 -0300

    editado tips2

--- Fundamentos 2/tips2.js
+++ Fundamentos 2/tips2.js
@@ -1,2 +1,20 @@
 'use strict'
 
+function calcularGorjeta(valor) {
+    if (valor >= 50 && valor <= 300) {
+        const tip = valor * 0.15;
+        return tip;
+    } else {
+        const tip = valor * 0.20;
+        return tip;
+    }
+}
+
+let tip = calcularGorjeta(350)
+console.log(tip);
+
+const contas = [125, 555, 44]
+const gorjetas = [calcularGorjeta(contas[0]), calcularGorjeta(contas[1]), calcularGorjeta(contas[2])];
+console.log(gorjetas);
+const totais = [contas[0] + gorjetas[0], contas[1] + gorjetas[1], contas[2] + gorjetas[2]]
+console.log(totais);
