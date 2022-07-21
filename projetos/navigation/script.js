ee169a2920e574329e3610fba1fd0a28d7212693
Victor Schiavo <schiavovictor@outlook.com>
2022-07-20 23:29:17 -0300

    Removido animações

--- projetos/navigation/script.js
+++ projetos/navigation/script.js
@@ -1,6 +1,6 @@
 const burguer = document.querySelector('.burguer');
 const navBar = document.querySelector('.navbar');
-const burguerFechar = document.querySelector('.fa-xmark')
+const burguerFechar = document.querySelector('.closeburguer')
 const header = document.querySelector('header')
 
 burguer.addEventListener('click', abrirMenu);
@@ -9,7 +9,6 @@ burguerFechar.addEventListener('click', abrirMenu)
 
 
 function abrirMenu() {
-    header.classList.toggle('nav-aberto');
-    navBar.classList.toggle('nav-aberto');
-    navBar.classList.toggle('tilt')
-};
+    navBar.classList.toggle('nav-aberto')
+    header.classList.toggle('nav-aberto')
+}
